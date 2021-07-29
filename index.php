818922e6f7fe38eef3e2c8ec704b924e8a4f98a1
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-07-29 17:40:45 -0500

    connect

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
     $dbUser = 'nikkenla_mkrt';
     $dbPass = 'NNikken2011$$';
 
-    try {
+    /*try {
         $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(Exception $e){
         exit("Error al conectar: " . $e->getMessage());
-    }
+    }*/
 
-    /*$nikkenla_marketingConnect = new mysqli("172.24.16.75:3306", "nikkenla_mkrt", "NNikken2011$$", "nikkenla_marketing");
-    if ($nikkenla_marketingConnect->connect_errno) {
-        echo "Fallo al conectar a MySQL<br>";
+    $nikkenla_marketingConnect = new mysqli("172.24.16.75:3306", "nikkenla_mkrt", "NNikken2011$$", "nikkenla_marketing");
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
     else{
         echo "conectado";
-    }*/
+    }
