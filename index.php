466bba3aa0a8ebcf348dfdce2d8b036c17d462f7
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-07-28 20:15:48 -0500

    28/07/2021

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-	echo "Hola mundo";
+	$nikkenla_marketingConnect = new mysqli("104.238.83.157", "nikkenla_mkrt", "NNikken2011$$", "nikkenla_marketing");
+    if ($nikkenla_marketingConnect->connect_errno) {
+        return "Fallo al conectar a MySQL<br>";
+    }
+    else{
+        echo "conectado";
+    }
