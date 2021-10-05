22f4b9d815227c7257b8ebd42eaf121545aa7a7b
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-10-05 12:03:55 -0500

    05/10/2021

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-	$dbHost = '104.130.46.73:3306';
-    $dbName = 'nikkenla_site';
-    $dbUser = 'nikkenla_mkrt';
-    $dbPass = 'NNikken2011$$';
+	$dbHost = '52.52.67.160:3306';
+    $dbName = 'mitiendanikken';
+    $dbUser = 'forge';
+    $dbPass = '8L8xQ1O9l6cVZMBtBBKS';
 
     try {
         $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "conectado";
+        echo "conectado a la TV";
     }
     catch(Exception $e){
         exit("Error al conectar: " . $e->getMessage());
