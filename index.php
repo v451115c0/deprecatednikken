3d1abecb876ce72fa0bb8db5af31784dfc33bfa0
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-11-18 16:10:13 -0600

    18/11/2021

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-	phpinfo();
+	ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+    $ip_conecction = "200.66.68.164";
+    $db_conecction = "NIKKENCOL";
+    
+    $connectionInfo = array("Database" => $db_conecction, "UID" => "nikkenmk", "PWD" => "M4rk3t1n$");
+    $conn = sqlsrv_connect($ip_conecction, $connectionInfo);
+    if(!$conn){ die(print_r(sqlsrv_errors(), true)); }
+    
+    $counter = 0;
+    
+    $query = "SELECT TOP 1 * FROM OOCRD";
+    $recordSet = sqlsrv_query($conn, $query) or die( print_r( sqlsrv_errors(), true));
+    while ($row = sqlsrv_fetch_array($recordSet, SQLSRV_FETCH_ASSOC)){
+        echo "Hola";
+    }
