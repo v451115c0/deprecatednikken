aae17dd47b9eb2567b75fbcc50cd96f7c404545b
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-11-18 16:04:06 -0600

    18/11/2021

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-	$serverName = "200.66.68.164\sqlexpress"; //serverName\instanceName
-    $connectionInfo = array( "Database"=>"NIKKENCOL", "UID"=>"nikkenmk", "PWD"=>"M4rk3t1n$");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-    
-    if( $conn ) {
-         echo "Conexión establecida.<br />";
-    }else{
-         echo "Conexión no se pudo establecer.<br />";
-         die( print_r( sqlsrv_errors(), true));
-    }
+	phpinfo();
