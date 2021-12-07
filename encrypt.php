c8cba6569272cb0dd48090fe73fde1231c342825
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2021-12-07 10:44:40 -0600

    encrypt

--- encrypt.php
+++ encrypt.php
@@ -14,6 +14,6 @@
 
 	//Encrypt data
 	$encData = mcrypt_encrypt('tripledes', $key, $data, 'ecb');
-    exit($encData);
+    //exit($encData);
 	header ("Location: https://facturacionmex.nikkenlatam.com:8383/wfrmLogin.aspx?pId=".base64_encode($encData));
 ?>
